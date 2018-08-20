870ec88c8d36c9be70f36368a39a8f8f574f0293
Dario Aburto <spis18aa@its-cseb230-20.ucsd.edu>
2018-08-20 15:14:36 -0700

    DA/MU WIP Geometric transformations.

--- imaging.py
+++ imaging.py
@@ -76,6 +76,65 @@ def flipVert(im):
             im.putpixel((x, height - y - 1), (red, green, blue))
             im.putpixel((x, y), (red2, green2, blue2))
 
+
+def scale(im):
+    ''' Make mirror image of left of a picture '''
+    # Find the dimensions of the image
+    (width, height) = im.size
+    newIm  = Image.new('RGB', ( width//2, height//2))
+
+    # Loop over the entire image
+    for x in range(width):
+        for y in range(height):
+            (red, green, blue) = im.getpixel((x, y))
+
+            newIm.putpixel((x//2, y//2), (red, green, blue))
+    return newIm
+            
+def blur(im):
+    ''' Make mirror image of left of a picture '''
+    # Find the dimensions of the image
+    (width, height) = im.size
+    newIm  = Image.new('RGB', ( width, height))
+
+    # Loop over the entire image
+    for x in range(1, width - 1, 2):
+        for y in range(1, height - 1, 2):
+            (redNW, greenNW, blueNW) = im.getpixel((x-1, y-1))
+            (redN, greenN, blueN) = im.getpixel((x, y-1))
+            (redNE, greenNE, blueNE) = im.getpixel((x+1, y-1))
+            (redW, greenW, blueW) = im.getpixel((x-1, y))
+            (red, green, blue) = im.getpixel((x, y))
+            (redE, greenE, blueE) = im.getpixel((x+1, y))
+            (redSW, greenSW, blueSW) = im.getpixel((x-1, y+1))
+            (redS, greenS, blueS) = im.getpixel((x, y+1))
+            (redSE, greenSE, blueSE) = im.getpixel((x+1, y+1))
+
+            newRed = (redNW + redN + redNE + redW + red + redE + redSW + redS + redSE) //9
+            newGreen = (greenNW + greenN + greenNE + greenW + green + greenE + greenSW + greenS + greenSE) //9
+            newBlue = (blueNW + blueN + blueNE + blueW + blue + blueE + blueSW + blueS + blueSE) //9
+
+
+            newIm.putpixel((x-1, y-1), (newRed, newGreen, newBlue))
+            newIm.putpixel((x, y-1), (newRed, newGreen, newBlue))
+            newIm.putpixel((x+1, y-1), (newRed, newGreen, newBlue))
+            newIm.putpixel((x-1, y), (newRed, newGreen, newBlue))
+            newIm.putpixel((x, y), (newRed, newGreen, newBlue))
+            newIm.putpixel((x+1, y), (newRed, newGreen, newBlue))
+            newIm.putpixel((x-1, y+1), (newRed, newGreen, newBlue))
+            newIm.putpixel((x, y+1), (newRed, newGreen, newBlue))
+            newIm.putpixel((x+1, y+1), (newRed, newGreen, newBlue))
+    return newIm
+            
+
+
+
+
+
+
+
+
+
 ######################### FUNCTIONS DEFINED #########################
 
 # Opens the "bear.jpg" file to RAM and creates a pointer to it at "bear"
@@ -112,3 +171,12 @@ bear = Image.open("bear.jpg")
 # Displays the image that "bear" is pointing to
 flipVert(bear)
 bear.show()
+
+# Resets bear
+bear = Image.open("bear.jpg")
+bearScale= scale(bear)
+bearScale.show()
+
+# Resets bear
+bearBlur = blur(bear)
+bearBlur.show()
